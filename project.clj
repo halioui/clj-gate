e12456516c6a4233369474423682f82085dfab77
Žygimantas Medelis <zzygis@gmail.com>
2014-05-19 14:16:21 +0300

    Update gate to v8

--- project.clj
+++ project.clj
@@ -1,12 +1,8 @@
-(defproject clj-gate "0.5.1"
-  :description "FIXME: write"
-  :dependencies[[enlive "1.1.1"]
+(defproject clj-gate "0.5.2"
+  :description "GATE embedded wrappers"
+  :dependencies[[enlive "1.1.5"]
                 [commons-io "1.4"]
                 [commons-lang "2.3"]
-                [com.thoughtworks.xstream/xstream "1.4.2"]
-                [uk.ac.gate/gate-core "7.1"]
-                [org.apache.tika/tika-parsers "1.2"]
-                [nekohtml "1.9.6.2"]
-                [org.clojure/clojure "1.6.0"]]
-  :exclusions [de.l3s.boilerpipe/boilerpipe 
-               org.apache.lucene/lucene-contrib])
+                [uk.ac.gate/gate-core "8.0"]
+                [org.apache.tika/tika-parsers "1.5"]
+                [org.clojure/clojure "1.6.0"]])
