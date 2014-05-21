103d573b4c19b13ce1518590730f7b0a4e0314ce
Žygimantas Medelis <zzygis@gmail.com>
2014-05-21 10:37:46 +0300

    Add testing for html parse

--- test/gate/test/document_test.clj
+++ test/gate/test/document_test.clj
@@ -2,14 +2,23 @@
   (:require [gate.document :refer :all]
             [midje.sweet :refer :all]
             [gate.controller :refer [gate-init]]
-            [clojure.java.io :as io]))
+            [clojure.java.io :refer :all]))
 
 (def txt (slurp "test/data/short.txt"))
 
 (gate-init)
 
 (facts "about document building"
-  (fact "build doc from string"
-        (text (build-doc txt)) => txt
-        (text (build-doc (io/as-file "test/data/short.txt"))) => txt
-        (text (build-doc (io/as-url "file:./test/data/short.txt"))) => txt))
+  (fact "build doc from string" (text (build-doc txt)) => txt)
+  (fact "build doc from file"   (text (build-doc (as-file "test/data/short.txt"))) => txt)
+  (fact "build doc from url"    (text (build-doc (as-url "file:./test/data/short.txt"))) => txt))
+
+
+(facts "about parsing non-text docs"
+  (fact "get original markups from html"
+        (-> "test/data/short.html"
+          as-file
+          build-doc
+          (.getAnnotations "Original markups")
+          count)
+        => 6))
