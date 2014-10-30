7f464b234ce8ae374f025beb4670f8c4cbd9b780
Žygimantas Medelis <zzygis@gmail.com>
2014-10-30 15:39:46 +0200

    Set source url to new document

--- src/gate/document.clj
+++ src/gate/document.clj
@@ -2,7 +2,7 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as s]
             [net.cgrand.enlive-html :as html])
-  (:import 
+  (:import
      (gate Annotation AnnotationSet Document Utils Factory)
      (gate.corpora DocumentImpl)
      (gate.util InvalidOffsetException)))
@@ -13,12 +13,18 @@
 
 (defn- clean[t] (-> (s/replace t #"\s+" " ") s/trim s/trim-newline))
 
-(defn clean-text 
+(defn clean-text
   ([document annotation] (clean  (text document annotation)))
   ([document] (clean (text document (Utils/start document) (Utils/end document)))))
 
 (defmulti build-doc class)
-(defmethod build-doc String [f] (Factory/newDocument f))
-(defmethod build-doc java.io.File [f] (Factory/newDocument (slurp f)))
-(defmethod build-doc java.net.URL [f] (Factory/newDocument f))
 
+(defmethod build-doc String [f] (Factory/newDocument f))
+
+(defmethod build-doc java.io.File [f]
+  (doto (Factory/newDocument (slurp f))
+    (.setSourceUrl (-> f .toURI .toURL))))
+
+(defmethod build-doc java.net.URL [f]
+  (doto (Factory/newDocument (slurp f))
+    (.setSourceUrl f)))
